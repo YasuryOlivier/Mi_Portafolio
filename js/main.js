c9a29130a6dbef4057b49cd3c4d9182d72c98299
YasuryOlivier <olivierbarriosyasury@gmail.com>
2016-11-27 14:50:33 -0600

    coloque imagenes

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ $( document).ready(function(){
 	$(".button-collapse").sideNav();
 	  //$('.materialboxed').materialbox();
 	 $('.slider').slider({full_width: true});
+	$('.parallax').parallax();
 });
